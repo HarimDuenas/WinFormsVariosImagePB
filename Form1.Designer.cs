1bd38bfb3ad1fbfeec8f96b6c95b272165c00251
Harim Dueñas <harimenriqueduenas@outlook.com>
2024-11-23 20:50:23 -0600

    agregar el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WinFormsVariosImagePB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            buttonAbrir = new Button();
+            labelNombre = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(27, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(151, 139);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(194, 25);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(151, 139);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(367, 25);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(151, 139);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(27, 188);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(151, 139);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(194, 188);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(151, 139);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(367, 188);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(151, 139);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // buttonAbrir
+            // 
+            buttonAbrir.BackColor = SystemColors.Desktop;
+            buttonAbrir.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonAbrir.ForeColor = Color.White;
+            buttonAbrir.Location = new Point(158, 358);
+            buttonAbrir.Name = "buttonAbrir";
+            buttonAbrir.Size = new Size(239, 59);
+            buttonAbrir.TabIndex = 6;
+            buttonAbrir.Text = "Abrir";
+            buttonAbrir.UseVisualStyleBackColor = false;
+            buttonAbrir.Click += buttonAbrir_Click;
+            // 
+            // labelNombre
+            // 
+            labelNombre.AutoSize = true;
+            labelNombre.Font = new Font("YouYuan", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            labelNombre.Location = new Point(12, 435);
+            labelNombre.Name = "labelNombre";
+            labelNombre.Size = new Size(497, 20);
+            labelNombre.TabIndex = 7;
+            labelNombre.Text = "Harim Jesus Enrique Dueñas Davila -------> 442063";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(549, 464);
+            Controls.Add(labelNombre);
+            Controls.Add(buttonAbrir);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private Button buttonAbrir;
+        private Label labelNombre;
+    }
+}
